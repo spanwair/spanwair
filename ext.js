677217cc2b917d03ef94c16679daef6929d4bb2a
Jan Gabriel <jan.gabriel@snowlycode.com>
2020-07-08 00:18:32 +0200

    use fetch instead of axios

--- ext.js
+++ ext.js
@@ -1,1 +1,31 @@
-(async()=>{const a=document.getElementById("changeble"),t=await axios.get("under_preparation.json"),o=await axios.get("country_language.json"),n=await axios.get("https://geolocation-db.com/json/");if(t&&n&&t.data&&n.data&&n.data.country_code){const e=n.data.country_code||"",d=o.data[e]||"GB",c=t.data.find(a=>a.symbol===d);c&&(a.innerText=c.text)}})();
+(async () => {
+  const f = async (url) => {
+    try {
+      return await fetch(url)
+        .then(r => r.json());
+    } catch (err) {
+      console.log('err :>> ', err);
+      return [];
+    }
+  }
+  const ch = document.getElementById('changeble');
+  let uP, cL, iP;
+  await Promise.all([
+    f('under_preparation.json').then(r=>uP=r).catch(err=>console.log('err :>> ', err)),
+    f('country_language.json').then(r=>cL=r).catch(err=>console.log('err :>> ', err)),
+    f('https://geolocation-db.com/json/').then(r=>iP=r).catch(err=>console.log('err :>> ', err)),
+  ])
+  .then(() => {
+    if (!!uP && !!cL && !!iP) {
+      if (!!iP.country_code) {
+        const c = iP.country_code || '';
+        const fL = cL[c] || 'GB';
+        const fI = uP.find(u => u.symbol === fL);
+        if (!!fI) {
+          ch.innerText = fI.text;
+        }
+      }
+    }
+  })
+  .catch(err => console.log('err :>> ', err));
+})();
